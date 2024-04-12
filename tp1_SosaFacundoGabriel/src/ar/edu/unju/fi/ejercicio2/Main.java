83a219d5583d49e5540d3260072e532fe585a897
SOSA <SOSA@DESKTOP-V4PQRV0>
2024-04-12 19:50:33 -0300

    Ejercicio 2

---
+++ tp1_SosaFacundoGabriel/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String pais = "Argentina";
+        int edad = 30;
+        double altura = 450; 
+        double precio = 1200; 
+        String numeroTelefono = "0388151000000";
+        double coseno = Math.cos(0.5);
+
+        System.out.println("Nombre del país: " + pais);
+        System.out.println("Edad de la persona: " + edad + " años");
+        System.out.println("Altura del edificio: " + altura + " metros");
+        System.out.println("Precio del producto: $" + precio);
+        System.out.println("Número de teléfono: " + numeroTelefono);
+        System.out.println("Coseno de 0.5: " + coseno);
+	}
+
+}
