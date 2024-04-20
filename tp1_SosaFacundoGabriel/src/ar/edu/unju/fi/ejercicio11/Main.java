a1913f226bb75e184821c4619f3f47dc28dd90e6
SOSA <SOSA@DESKTOP-V4PQRV0>
2024-04-20 15:32:40 -0300

    Ejercicio 11

---
+++ tp1_SosaFacundoGabriel/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 2;
+		int j;
+		
+		for(j = 40; j > 0; j--) {
+			System.out.print(j*i + " ");
+			i++;
+		}
+	}
+
+}
